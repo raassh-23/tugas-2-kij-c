1845f9011798c8fd6243b3c9aea46f920fa66ae0
fiansyahm <fiansyahmm@gmail.com>
2022-10-29 16:29:59 +0700

    Update sign-document.py

--- sign-document.py
+++ sign-document.py
@@ -1,10 +1,13 @@
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+import codecs
 
 class sign_document:
-    def __init__(self,message,file_location):
-        self.key=RSA.import_key(open(file_location).read())
+    def __init__(self,file_pdf,file_private_key):
+        with open(file_pdf, 'rb') as fp:
+            message = fp.read()
+        self.key=RSA.import_key(open(file_private_key).read())
         self.h = SHA256.new(message)
 
     def sign(self):
@@ -17,13 +20,12 @@ class sign_document:
         except (ValueError, TypeError):
             print ("The signature is not valid.")
 if __name__ == '__main__':
-    message = b'To be signed'
-    file_location='./output/private.pem'
-    
-    rsa=sign_document(message,file_location)
+    file_private_key='./output/private.pem'
+    file_pdf="seni.pdf"
+    rsa=sign_document(file_pdf,file_private_key)
     signature=rsa.sign()
+    print("Signature:")
     print(signature)
-    
     rsa.verify(signature)
     
 
