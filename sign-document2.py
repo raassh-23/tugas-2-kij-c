e0d398a026fcf73395ada15de0bea7f2d5594a17
fiansyahm <fiansyahmm@gmail.com>
2022-10-29 16:57:52 +0700

    .

--- sign-document2.py
+++ sign-document2.py
@@ -20,14 +20,22 @@ class sign_document:
     #     except (ValueError, TypeError):
     #         print ("The signature is not valid.")
 if __name__ == '__main__':
-    file_private_key='./output/private.pem'
-    file_pdf="seni.pdf"
-    rsa=sign_document(file_pdf,file_private_key)
-    signature=rsa.sign()
-    print("Signature:")
-    print(signature)
-    # rsa.verify(signature)
+    from Crypto.PublicKey import RSA
 
-    
+    keyPair = RSA.generate(bits=1024)
+    print(f"Public key:  (n={hex(keyPair.n)}, e={hex(keyPair.e)})")
+    print(f"Private key: (n={hex(keyPair.n)}, d={hex(keyPair.d)})")
+    # RSA sign the message
+    msg = b'A message for signing'
+    from hashlib import sha512
+    hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+    signature = pow(hash, keyPair.d, keyPair.n)
+    print("Signature:", hex(signature))
+
+    # RSA verify signature
+    msg = b'A message for signing'
+    hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+    hashFromSignature = pow(signature, keyPair.e, keyPair.n)
+    print("Signature valid:", hash == hashFromSignature)
     
 
