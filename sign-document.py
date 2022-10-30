843465b89e1a3f627b3811ff73029e05fb7af0ee
fiansyahm <fiansyahmm@gmail.com>
2022-10-30 12:50:54 +0700

    revisi

--- sign-document.py
+++ sign-document.py
@@ -1,32 +1,30 @@
 from Crypto.Signature import pkcs1_15
-from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-import codecs
+from Crypto.Cipher import PKCS1_OAEP
 
 class sign_document:
-    def __init__(self,file_pdf,file_private_key):
-        with open(file_pdf, 'rb') as fp:
-            message = fp.read()
-        self.key=RSA.import_key(open(file_private_key).read())
-        self.h = SHA256.new(message)
-
-    def sign(self):
-        signature = pkcs1_15.new(self.key).sign(self.h)
-        return signature
-    # def verify(self,signature):
-    #     try:
-    #         pkcs1_15.new(self.key).verify(self.h, signature)
-    #         print("The signature is valid.")
-    #     except (ValueError, TypeError):
-    #         print ("The signature is not valid.")
+    def __init__(self,file_private_key,file_public_key):
+        key = RSA.importKey(open(file_public_key).read())
+        self.cipher_public = PKCS1_OAEP.new(key)
+        key = RSA.importKey(open(file_private_key).read())
+        self.cipher_private = PKCS1_OAEP.new(key)
+    def encrypt(self,message):
+        ciphertext = self.cipher_public.encrypt(message)
+        return ciphertext 
+    def decrypt(self,message):
+        plaintext = self.cipher_private.decrypt(message)
+        return plaintext 
 if __name__ == '__main__':
     file_private_key='./output/private.pem'
-    file_pdf="seni.pdf"
-    rsa=sign_document(file_pdf,file_private_key)
-    signature=rsa.sign()
-    print("Signature:")
-    print(signature)
-    # rsa.verify(signature)
+    file_public_key='./output/public.pem'
+    # file_pdf="seni.pdf"
+    # with open(file_pdf, 'rb') as fp:
+    #     message = fp.read()
+    message = b'You can attack now!'
+    rsa=sign_document(file_private_key,file_public_key)   
+    ciphertext = rsa.encrypt(message)
+    message = rsa.decrypt(ciphertext)
+    print(message)
 
     
     
